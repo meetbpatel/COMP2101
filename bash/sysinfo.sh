3e9a746a710537308bcc34fb8bbe2548a44bf109
Meet Patel <meepatel0212@gmail.com>
2022-06-09 03:42:56 -0400

    Updating in format as per lab-2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,19 +2,19 @@
 #Student Name: Meet Bharatbhai Patel
 #Student Number: 200465967
 
-
 hs=$(hostname)
-echo "Host name: $hs"
-
-dn=$(hostname --d)
-echo "Domain name: $dn"
-
 un=$(uname)
 unv=$(uname -r)
-echo -e "Operating System name and version: \n$un and $unv"
-
 ip=$(hostname -I)
-echo "IP Address: $ip"
+fs=$(du -sh)
 
-fs=$(df -Th | grep "^/dev")
-echo "$fs"
+cat <<EOF
+
+Report for myvm($hs)
+=========================
+FQDN: $hs
+Operating System name and version: $un and $unv
+IP Address: $ip
+Root Filesystem Free Space: $fs
+=========================
+EOF
