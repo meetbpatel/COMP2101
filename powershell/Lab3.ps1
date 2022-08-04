e53d171304c1495cc35b464b60cb2a27cc8ad984
meetbpatel <105845509+meetbpatel@users.noreply.github.com>
2022-08-03 23:33:47 -0400

    Lab3
    Lab3

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,3 @@
+﻿Get-WmiObject -Class Win32_NetworkAdapterConfiguration | 
+Where-Object -Filter {("IPEnabled=True")} | 
+Format-Table -Property Description,Index,IPAddress,DefaultIPGateway,DNSDomain,ServiceName
