aa06e1fcaf76645220374effb102c18a0d34145d
meetbpatel <105845509+meetbpatel@users.noreply.github.com>
2022-08-18 03:51:25 -0400

    Update Lab4.ps1
    Update-Modified as per assignment

--- powershell/Lab4.ps1
+++ powershell/Lab4.ps1
@@ -1,109 +1,51 @@
-﻿#Hardware discription 
-#$hardwarediscription = Get-WmiObject Win32_computersystem
-Write-output "----Hardware Description----"
-write-output (Get-WmiObject Win32_computersystem) `n
+﻿Function Lab{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True,
+                    ValueFromPipeline=$true,
+                    ValueFromPipelineByPropertyName=$true)]
+        [String[]]$ComputerName="."
+    )
+    Begin{}
+    Process{
+        foreach($Computer in $ComputerName){
+	    $cpu=Get-WmiObject Win32_processor 
+            $os=Get-Wmiobject Win32_OperatingSystem 
+	    $ram=Get-WmiObject Win32_physicalmemory
+	    $video=Get-WmiObject Win32_videocontroller
+	    $network=Get-WmiObject -Class Win32_NetworkAdapterConfiguration
+            
+	    write-output "****** -System, -Disk, -Network ******"
 
-#OPerating system
-Write-output "----Operating System Name----"
-write-output (Get-WmiObject Win32_OperatingSystem).Caption `n
-
-
-#Processor Details
-
-$processordetails = Get-WmiObject Win32_processor
-if(($processordetails).MaxClockSpeed -ne $null)
-{
-	Write-output "----Processor Clock Speed Details----"
-	Write-output "MaxClockSpeed" 
-	Write-output "--------------" ($processordetails).MaxClockSpeed `n
-} 
-if((Get-WmiObject Win32_processor).NumberOfCores -ne $null)
-{
-	Write-output "----Processor Number of Cores Details----"
-	Write-output "Number of Cores"	
-	Write-output "----------------" (Get-WmiObject Win32_processor).NumberOfCores `n
-} 
-if((Get-WmiObject Win32_processor).L1CacheSize -eq $null)
-{
-	Write-output "----Processor L1 Cache Size----"
-	Write-output "L1 Cache Size"
-	Write-output "--------------" (Get-WmiObject Win32_processor).L1CacheSize `n
-}
-if(($processordetails).L2CacheSize -eq $null)
-{
-	Write-output "----Processor L2 Cache Size----"
-	Write-output "L2 Cache Size"
-	Write-output "--------------" ($processordetails).L2CacheSize `n
-}
-if(($processordetails).L3CacheSize -ne $null)
-{
-	Write-output "----Processor L3 Cache Size----"
-	Write-output "L3 Cache Size"
-	Write-output "--------------" ($processordetails).L3CacheSize `n
-}
-
-
-$physicalmemory = Get-WmiObject Win32_physicalmemory
-if((Get-WmiObject Win32_physicalmemory).Manufacturer -ne $null)
-{
-	Write-output "----Physical Memory Vendor----"
-	Write-output "Manufacturer(Vendor)"
-	Write-output "--------------------" ($physicalmemory).Manufacturer `n
-}
-if((Get-WmiObject Win32_physicalmemory).Description -ne $null)
-{
-	Write-output "----Physical Memory Description----"
-	Write-output "Description"
-	Write-output "------------" ($physicalmemory).Description `n
-}
-if((Get-WmiObject Win32_physicalmemory).Capacity -ne $null)
-{
-	Write-output "----Physical Memory Size----"
-	Write-output "Size(Capacity)"
-	Write-output "-----" ($physicalmemory).Capacity `n
-}
-if((Get-WmiObject Win32_physicalmemory).BankLabel -ne $null)
-{
-	Write-output "----Physical Memory BankLabel----"
-	Write-output "BankLabel"
-	Write-output "----------" ($physicalmemory).BankLabel `n
+            $presenting=[ordered] @{ #With or without [ordered]
+                'Name'=$Computer;
+		'CPU_mxclkspeed'=$cpu.MaxClockSpeed;
+	 	'cpu_NumberOfCores'=$cpu.NumberOfCores;
+		'cpu_L1cachesize'=$cpu.L1CacheSize;
+		'cpu_L2cachesize'=$cpu.L2CacheSize;
+		'cpu_L3cachesize'=$cpu.L3CacheSize;
+                'OS'=$os.caption;
+                'videoname'=$video.Name;
+		'videodescription'=$video.Description;
+		'rammanufacturer'=$ram.Manufacturer;
+		'ramdescription'=$ram.Description;
+		'ramsize(capacity)'=$ram.Capacity;
+		'rambanklabel'=$ram.BankLabel;
+		'networkdescription'=$network.Description;
+		'networkIndex'=$network.Index;
+		'networkIPAddress'=$network.IPAddress;
+		'networkdefaultgateway'=$network.DefaultIPGateway;
+		'networkDNSdomain'=$network.DNSDomain;
+		'networkServicename'=$network.ServiceName
+		
+            }
+        $createnewobject=New-Object -TypeName PSObject -Property $presenting
+        Write-Output $createnewobject	
+        } 
+    }
+    End{}
 }
 
 
-#Network Adapter Configuration Lab3
-Write-output "----Network Adapter Configuration Lab3----"
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration | 
-Where-Object -Filter {("IPEnabled=True")} | 
-Format-Table -Property Description,Index,IPAddress,DefaultIPGateway,DNSDomain,ServiceName
-
-
-#Video Card Details
-$videocontroller = Get-WmiObject Win32_videocontroller
-foreach ($vc in $videocontroller) {
-      $vcname = $vc| -Property Name
-      foreach ($vcd in $vcname) {
-            $vcdesc = $vcd | -Property Description
-            foreach ($ld in $lcd) {
-                     new-object -typename psobject -property @{Name=$vc.Name                                                               
-                                                               Description=$ld.Description
-                                                               Resouluation=$lcd.VideoModeDescription
-                                                               }
-           }
-      }
-  }
-if(($videocontroller).Name -ne $null)
-{
-	Write-output "----Video Control----"
-	Write-output "Name"
-	Write-output "-----" ($videocontroller).Name `n
-}
-if(($videocontroller).Description -ne $null)
-{
-	Write-output "Description"
-	Write-output "------------" ($videocontroller).Description `n
-}
-if(($videocontroller).VideoModeDescription -ne $null)
-{
-	Write-output "Resoluation"
-	Write-output "------------" ($videocontroller).VideoModeDescription `n
-}
+$informationmodified = Lab DESKTOP-FFGJRUF
+$informationmodified
